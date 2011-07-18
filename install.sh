f62bc06641dcacffaac8008c27321c4bb68a5637
Rafael V. dos Santos <rvsantos@live.com>
2011-07-18 20:48:01 -0300

    Removendo CommandT do install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ git clone --recursive git://github.com/Geekquilibrium/dotfiles.git
 echo "Linkando arquivos na home\n"
 source ~/dotfiles/etc/link.sh
 
-echo "Compilando CommandT\n"
-cd ~/.vim/bundle/commandt
-(rake make) || warn "Ruby compilation failed."
+#echo "Compilando CommandT\n"
+#cd ~/.vim/bundle/commandt
+#(rake make) || warn "Ruby compilation failed."
